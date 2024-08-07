a9174495e8a4082a56d175c87ee437cd76bd99e7
dweinholz <david-weinholz@web.de>
2024-08-07 15:53:06 +0200

    updated metadata script

--- simple_vm_client/openstack_connector/scripts/bash/save_metadata_auth_token.sh
+++ simple_vm_client/openstack_connector/scripts/bash/save_metadata_auth_token.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Ensure that METADATA_AUTH_TOKEN is properly escaped
+TOKEN_ESCAPED=$(printf '%q' "$METADATA_AUTH_TOKEN")
 
 # Create a configuration file with the token
-echo "METADATA_ACCESS_TOKEN='$METADATA_AUTH_TOKEN'" > /etc/metadata_config.env
+echo "METADATA_ACCESS_TOKEN='$TOKEN_ESCAPED'" > /etc/metadata_config.env
 
 # Secure the file permissions
 chmod 600 /etc/metadata_config.env
